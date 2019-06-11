6e6426c1637722c0fa2d75a17d96d70b358c87d2
li420115 <lyq420115>
2019-06-11 08:49:45 +0800

    李云强-6-11

---
+++ liyunqiang/后台管理/js/钟表.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: '#zb',
+    data: {
+        hour: new Date().getHours() * 30,
+        miute: new Date().getMinutes() * 6,
+        second: new Date().getSeconds() * 6
+
+    },
+    created() {
+        this.dida()
+    },
+
+
+    methods: {
+        dida() {
+            setInterval(() => {
+                this.hour = new Date().getHours() * 30,
+                    this.miute = new Date().getMinutes() * 6,
+                    this.second = new Date().getSeconds() * 6
+
+            }, 1000);
+        },
+
+    }
+
+})
